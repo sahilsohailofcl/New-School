0c321db6be556f40918d01e197d5fee07c491145
sahilsohailofcl <sahilsohailofcl@gmail.com>
2024-09-19 09:52:41 +0500

    add formating

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-import './main.css'
-import './js/function.js'
+window.addEventListener('scroll', function() {
+    const heroGrid = document.querySelector('.hero-grid-1');
+    
+    if (window.scrollY > 100) {
+      heroGrid.classList.add('swapped');
+    } else {
+      heroGrid.classList.remove('swapped');
+    }
+  });
+  
+
+window.addEventListener('scroll', function() {
+    const heroGrid = document.querySelector('.hero-grid-3');
+    
+    if (window.scrollY > 100) {
+      heroGrid.classList.add('swapped');
+    } else {
+      heroGrid.classList.remove('swapped');
+    }
+  });
