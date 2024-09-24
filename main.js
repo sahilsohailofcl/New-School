50c8812cf18a418692685b8fecf234c18a7a4f7c
sahilsohailofcl <sahilsohailofcl@gmail.com>
2024-09-24 10:51:15 +0500

    add styles

--- main.js
+++ main.js
@@ -9,15 +9,21 @@ headerMenu.addEventListener('click', function() {
 
 
 window.addEventListener('scroll', function() {
-    const heroGrid1 = document.querySelector('.hero-grid-1');
-    const heroGrid3 = document.querySelector('.hero-grid-3');
-  
-    if (window.scrollY > 100) {
-      heroGrid1.classList.add('swapped');
-      heroGrid3.classList.add('swapped');
-    } else {
+  const heroGrid1 = document.querySelector('.hero-grid-1');
+  const heroGrid3 = document.querySelector('.hero-grid-3');
+
+  if (window.innerWidth > 768) {
+      if (window.scrollY > 100) {
+          heroGrid1.classList.add('swapped');
+          heroGrid3.classList.add('swapped');
+      } else {
+          heroGrid1.classList.remove('swapped');
+          heroGrid3.classList.remove('swapped');
+      }
+  } else {
       heroGrid1.classList.remove('swapped');
       heroGrid3.classList.remove('swapped');
-    }
-  });
+  }
+});
+
  
