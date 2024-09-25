b75b35d18c3a1d0a4f0e98273de9959093693f80
sahilsohailofcl <sahilsohailofcl@gmail.com>
2024-09-25 10:40:11 +0500

    edit vercel

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
     const data = Object.fromEntries(formData.entries()); 
 
     try {
-        const response = await fetch('https://your-vercel-deployment-url.vercel.app/submit-form', {
+        const response = await fetch('https://newschool-pro.vercel.app/submit-form', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
