d4f509af0776b3a8999c02634b4bd5ebf30418e5
SAHIL SOAHIL <sahilsohailofcl@gmail.com>
2024-09-29 16:23:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -31,3 +31,31 @@ document.getElementById('contact-form').addEventListener('submit', async functio
         confirmationMessage.style.display = 'block';
     }
 });
+
+const headerMenu = document.getElementById('header-menu');
+const headerNav = document.getElementById('header-nav');
+
+headerMenu.addEventListener('click', function() {
+  if (window.innerWidth <= 768) {
+    headerNav.classList.toggle('active');
+  }
+});
+
+
+window.addEventListener('scroll', function() {
+  const heroGrid1 = document.querySelector('.hero-grid-1');
+  const heroGrid3 = document.querySelector('.hero-grid-3');
+
+  if (window.innerWidth > 768) {
+      if (window.scrollY > 100) {
+          heroGrid1.classList.add('swapped');
+          heroGrid3.classList.add('swapped');
+      } else {
+          heroGrid1.classList.remove('swapped');
+          heroGrid3.classList.remove('swapped');
+      }
+  } else {
+      heroGrid1.classList.remove('swapped');
+      heroGrid3.classList.remove('swapped');
+  }
+});
